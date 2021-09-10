95e4daee5f66a143db60353788241692636b7f2a
Findasoul <findasoul_jaydie@hotmail.com>
2021-09-10 18:45:06 +0700

    word_count

--- quiz1/wordcount/5_start_producer-harry.bat
+++ quiz1/wordcount/5_start_producer-harry.bat
@@ -1,1 +1,2 @@
-start cmd.exe /k "c:/chathai/env/Scripts/python.exe c:/chathai/quiz1/wordcount/producer-harry.py"
+@REM start cmd.exe /k "c:/chathai/env/Scripts/python.exe c:/chathai/quiz1/wordcount/producer-harry.py"
+START /b /wait cmd /C "C:\kafka_2.13-2.7.0\bin\windows\kafka-console-producer.bat --bootstrap-server "localhost:9092,localhost:9192,localhost:9292" --topic streams-harrycount-input"
